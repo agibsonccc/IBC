2fea4e72822b92c9f8f01f4a2e67d74596746cd8
Adam Gibson <adam@konduit.ai>
2022-01-23 17:57:23 +0900

    Maven refactor

---
+++ install-tws-launch-local.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=/home/agibsonccc/Jts/jars/twslaunch-1012.jar -DgroupId=com.interactivebrokers -DartifactId=twslaunch -Dversion=1012 -Dpackaging=jar
+mvn install:install-file -Dfile=/home/agibsonccc/Jts/jars/twslaunch-install4j-1.10.jar -DgroupId=com.interactivebrokers -DartifactId=twslaunch-install4j -Dversion=1.10 -Dpackaging=jar
