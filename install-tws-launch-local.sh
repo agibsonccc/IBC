93b80f287b7cff8688558772090a8340f093d255
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-01-27 08:58:54 +0900

    Update install-tws-launch-local.sh

--- install-tws-launch-local.sh
+++ install-tws-launch-local.sh
@@ -1,2 +1,3 @@
 mvn install:install-file -Dfile=/home/agibsonccc/Jts/jars/twslaunch-1012.jar -DgroupId=com.interactivebrokers -DartifactId=twslaunch -Dversion=1012 -Dpackaging=jar
 mvn install:install-file -Dfile=/home/agibsonccc/Jts/jars/twslaunch-install4j-1.10.jar -DgroupId=com.interactivebrokers -DartifactId=twslaunch-install4j -Dversion=1.10 -Dpackaging=jar
+mvn install:install-file -Dfile=/home/prod/Jts/1012/jars/jts4launch-1012.jar -DgroupId=com.interactivebrokers -DartifactId=jts4launch -Dversion=1012 -Dpackaging=jar
